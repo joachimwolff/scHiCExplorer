4b3aff0a38ea181a6b2bd45d3241c5e4d4bb5355
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-04-23 20:39:37 +0200

    update

--- schicexplorer/demultiplex_bash.sh
+++ schicexplorer/demultiplex_bash.sh
@@ -1,7 +1,8 @@
 for id in $(parsec  histories show_dataset_collection b4b580c4c286c45f 60311dd1b2959818 | jq .elements[].object.id -r); do 
-  parsec datasets download_dataset $id --file_path file.fastq --use_default_filename
+  parsec histories download_dataset $id --file_path file.fastq --use_default_filename
+#   parsec  histories download_dataset b4b580c4c286c45f e629965666ce4ce5 .
   # process
-  python scHiCDemultiplex -f file.fastq -s samples.txt -b GSE94489_README.txt -t 20
+  scHicDemultiplex -f file.fastq -s ../samples.txt -b ../GSE94489_README.txt -t 20
   # upload whatever via ftp
   curl -T "{$(echo *.gz | tr ' ' ',')}" ftp://galaxy.uni-freiburg.de -user wolffj@informatik.uni-freiburg.de:Password
   rm file.fastq
