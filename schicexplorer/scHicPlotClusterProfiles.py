51220aaf848848bb2469c05bb89e628c660d51a6
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-09-29 19:54:56 +0000

    Play around to get good looking density plot, failed so far :(

--- schicexplorer/scHicPlotClusterProfiles.py
+++ schicexplorer/scHicPlotClusterProfiles.py
@@ -62,6 +62,11 @@ def parse_arguments(args=None):
                                 help='Text file which contains per matrix the assoziated cluster.',
                                 metavar='cluster file',
                                 required=True)
+    parserRequired.add_argument('--loadFromNpz', '-npz',
+                                help='Load distance data from npz file.',
+                                # metavar='Create npz matrix or load it.',
+                                # required=True,
+                                action='store_false')
     parserRequired.add_argument('--maximalDistance',
                            help='maximal distance in bases',
                            required=False,
@@ -76,9 +81,26 @@ def parse_arguments(args=None):
 
     return parser
 
+def getIndex(pValue, pDistances):
 
+    # distances 10, 100, 1000, 10000
+    # for 10: 0,1, 2,3 ,4,5,6,7,8,9 --> same index pos
+    # for 100: merge 10 - 19 to one, 20-29 etc --> index pis 10, 11, 12
+    # for 1000: merge 100-199 to one etc --> index pos 20, 21
+    if pValue < pDistances[0]:
+        return pValue
+    for i, distance in enumerate(pDistances):
+        if pValue <= distance:
+            rest_factor = pValue // (pDistances[i-1])
+            ten_factor = np.floor(np.log10(pValue)) * 10
+            # log.debug('pValue {} {}'.format(np.log10(pValue)))
 
-def compute_read_distribution(pMatrixName, pMatricesList, pIndex, pXDimension, pQueue):
+            # log.debug('pValue {} int(ten_factor + rest_factor) {} {}'.format(pValue, ten_factor, rest_factor))
+            return int(ten_factor + rest_factor)
+    
+    return -1
+
+def compute_read_distribution(pMatrixName, pMatricesList, pIndex, pXDimension, pSteps, pQueue):
     read_coverage = []
     sparsity = []
     read_distribution = []
@@ -86,7 +108,7 @@ def compute_read_distribution(pMatrixName, pMatricesList, pIndex, pXDimension, p
     for i, matrix in enumerate(pMatricesList):
         matrixFileHandlerInput = MatrixFileHandler(pFileType='cool', pMatrixFile=pMatrixName+ '::' +matrix)
         _matrix, _, _, _, _ = matrixFileHandlerInput.load()
-        read_distribution_ = {}
+        
         # if neighborhood_matrix is None:
         #     neighborhood_matrix = csr_matrix((pXDimension, _matrix.shape[0] * _matrix.shape[1]), dtype=np.float)
 
@@ -96,12 +118,23 @@ def compute_read_distribution(pMatrixName, pMatricesList, pIndex, pXDimension, p
         relative_distance = np.absolute(instances - features)
         if max_length < max(relative_distance):
             max_length = max(relative_distance)
-        
-        for i, relative_distance_ in enumerate(relative_distance):
-            if relative_distance_ in read_distribution_:
-                read_distribution_[relative_distance_] += _matrix.data[i]
-            else:
-                read_distribution_[relative_distance_] = _matrix.data[i]
+        read_distribution_ = np.zeros(max_length+1)
+        for j, relative_distance_ in enumerate(relative_distance):
+            read_distribution_[relative_distance_] += _matrix.data[j]
+            # if relative_distance_ == 0:
+            #     index = 0
+            # else:
+            #     index = getIndex(relative_distance_, pSteps)
+            # if index == -1:
+            #     read_distribution_[-1] += _matrix.data[j]
+            #     continue
+            #     # cluster_array[index] = cluster_item[distance]
+            # if max_index < index:
+            #     max_index = index
+            # if index in read_distribution_:
+            #     read_distribution_[index] += _matrix.data[j]
+            # else:
+            #     read_distribution_[index] = _matrix.data[j]
 
         read_distribution.append(read_distribution_)
     
@@ -112,72 +145,95 @@ def main(args=None):
     args = parse_arguments().parse_args(args)
 
     create_or_load_matrix = False
-
-    # if args.createMatrix:
+    binSize = 10000
+    max_length = 100000000 # 100 mb
+    max_index = 0
+    steps = np.array([100000, 1000000, 10000000, 100000000 ])
+    steps = steps / binSize
+    if args.loadFromNpz:
         
-    matrices_name = args.matrix
-    threads = args.threads
-    matrices_list = cooler.fileops.list_coolers(matrices_name)
-    read_coverage = [None] * threads
-    sparsity = [None] * threads
+        matrices_name = args.matrix
+        threads = args.threads
+        matrices_list = cooler.fileops.list_coolers(matrices_name)
+        log.debug('matrices_list {}'.format(matrices_list[:10]))
+        read_coverage = [None] * threads
+        sparsity = [None] * threads
 
 
-    all_data_collected = False
-    thread_done = [False] * threads
-    log.debug('matrix read, starting processing')
-    length_index = [None] * threads
-    length_index[0] = 0
-    matricesPerThread = len(matrices_list) // threads
-    queue = [None] * threads
-    process = [None] * threads
-    max_length = 0
-    for i in range(threads):
-
-        if i < threads - 1:
-            matrices_name_list = matrices_list[i * matricesPerThread:(i + 1) * matricesPerThread]
-            length_index[i + 1] = length_index[i] + len(matrices_name_list)
-        else:
-            matrices_name_list = matrices_list[i * matricesPerThread:]
-
-        queue[i] = Queue()
-        process[i] = Process(target=compute_read_distribution, kwargs=dict(
-                            pMatrixName = matrices_name,
-                            pMatricesList= matrices_name_list, 
-                            pIndex = length_index[i], 
-                            pXDimension=len(matrices_list),
-                            pQueue=queue[i]
-            )
-        )
-
-        process[i].start()
-
-    while not all_data_collected:
+        all_data_collected = False
+        thread_done = [False] * threads
+        log.debug('matrix read, starting processing')
+        length_index = [None] * threads
+        length_index[0] = 0
+        matricesPerThread = len(matrices_list) // threads
+        queue = [None] * threads
+        process = [None] * threads
+        max_length = 0
         for i in range(threads):
-            if queue[i] is not None and not queue[i].empty():
-                read_coverage[i], max_length_ = queue[i].get()
-                if max_length < max_length_:
-                    max_length = max_length_
-                # read_coverage[i] = worker_result
-                # sparsity[i] = worker_result[1]
 
-                # if neighborhood_matrix is None:
-                #     neighborhood_matrix = csr_matrix_worker
-                # else:
-                #     neighborhood_matrix += csr_matrix_worker
+            if i < threads - 1:
+                matrices_name_list = matrices_list[i * matricesPerThread:(i + 1) * matricesPerThread]
+                length_index[i + 1] = length_index[i] + len(matrices_name_list)
+            else:
+                matrices_name_list = matrices_list[i * matricesPerThread:]
 
-                queue[i] = None
-                process[i].join()
-                process[i].terminate()
-                process[i] = None
-                thread_done[i] = True
-        all_data_collected = True
-        for thread in thread_done:
-            if not thread:
-                all_data_collected = False
-        time.sleep(1)
+            queue[i] = Queue()
+            process[i] = Process(target=compute_read_distribution, kwargs=dict(
+                                pMatrixName = matrices_name,
+                                pMatricesList= matrices_name_list, 
+                                pIndex = length_index[i], 
+                                pXDimension=len(matrices_list),
+                                pSteps=steps,
+                                pQueue=queue[i]
+                )
+            )
 
-    read_coverage = np.array([item for sublist in read_coverage for item in sublist])
+            process[i].start()
 
+        while not all_data_collected:
+            for i in range(threads):
+                if queue[i] is not None and not queue[i].empty():
+                    read_coverage[i], max_index_ = queue[i].get()
+                    if max_index < max_index_:
+                        max_index = max_index_
+                    # read_coverage[i] = worker_result
+                    # sparsity[i] = worker_result[1]
+
+                    # if neighborhood_matrix is None:
+                    #     neighborhood_matrix = csr_matrix_worker
+                    # else:
+                    #     neighborhood_matrix += csr_matrix_worker
+
+                    queue[i] = None
+                    process[i].join()
+                    process[i].terminate()
+                    process[i] = None
+                    thread_done[i] = True
+            all_data_collected = True
+            for thread in thread_done:
+                if not thread:
+                    all_data_collected = False
+            time.sleep(1)
+
+        log.debug('matrices processed')
+        read_distributions = []
+        for thread_data in read_coverage:
+            for matrix_data in thread_data:
+                # matrix_data_array = [0] * (max_index + 1)
+                # for key, value in matrix_data.items():
+                #     matrix_data_array[key] = value
+                read_distributions.append(matrix_data)
+        
+        read_distributions = np.array(read_distributions)
+        # read_coverage = np.array([item for sublist in read_coverage for item in sublist])
+        log.debug('one array computation done')
+        np.save('readcoverage_data.npz', np.array(read_distributions))
+
+    else:
+        log.debug('read file')
+        read_distributions = np.load('readcoverage_data.npz.npy', allow_pickle=True)
+
+    log.debug('Assoziating clusters')
     clusters = {}
     with open(args.clusters, 'r') as cluster_file:
 
@@ -186,11 +242,15 @@ def main(args=None):
             line_ = line.split(' ')[1]
             
             if int(line_) in clusters:
-                clusters[int(line_)].append(read_coverage[i])
+                clusters[int(line_)].append(read_distributions[i])
             else:
-                clusters[int(line_)] = [read_coverage[i]]
+                clusters[int(line_)] = [read_distributions[i]]
 
     cluster_to_plot = []    
+
+    log.debug('len(read_distributions) {}'.format(len(read_distributions)))
+    log.debug('len(read_distributions[0]) {}'.format(len(read_distributions[0])))
+
     # max_length = 0
     # for key in clusters:
     #     if max_length < max(clusters[key]):
@@ -220,14 +280,14 @@ def main(args=None):
 
     # for val, ax in zip(vals,grid):
 
-   
-    binSize = 1000000
 
+   
+    length_plot = 10 * len(steps)
     if (args.maximalDistance // binSize) < max_length:
 
         max_length = args.maximalDistance // binSize
     # fig, axes = plt.subplots(rows, columns, sharey=True)
-    
+    log.debug('Pooling data')
     clusters_list = []
     cluster_size = []
     for i, key in enumerate(clusters):
@@ -235,29 +295,40 @@ def main(args=None):
         
         # log.debug('cluster length {}'.format(len(clusters[key])))
         for cluster_item in clusters[key]:
-            cluster_array = np.zeros(max_length)
-            for distance in cluster_item.keys():
-                if distance < max_length:
-                    cluster_array[distance] = cluster_item[distance]
+            # cluster_array = np.zeros(max_length)
+            # log.debug('len(clusters[key]) {}'.format(len(clusters[key])))
+            # for distance in cluster_item.keys():
+               
+
+
+            #     if distance < max_length:
+            #     # log_value_distance = np.floor(np.log10(distance))
+            #     # if log_value_distance < 2:
+            #         cluster_array[distance] = cluster_item[distance]
+              
             
             # cluster_array.append(np.array(list(cluster_item.values()), dtype=np.int))
-            cluster_to_plot.append(cluster_array)
+            cluster_to_plot.append(cluster_item)
         # im = grid[i].imshow(np.log10(np.array(cluster_to_plot).T), cmap='hot', interpolation='nearest')
         clusters_list.append(cluster_to_plot)
         cluster_size.append(len(cluster_to_plot))
 
-    np.savez('cluster_data.npz', np.array(clusters_list))
+        # np.savez('cluster_data.npz', np.array(clusters_list))
+    # else:
+    #     clusters_list = np.load(args.matrix)
     cluster_size = np.array(cluster_size)
     # print(len(cluster_size))
     cluster_size = (1.0 - 0.1) * (cluster_size-min(cluster_size))/(max(cluster_size)-min(cluster_size)) + (0.1)
     # print(len(cluster_size))
     cluster_size = list(cluster_size)
+    log.debug('Plot data')
 
     # print(len(cluster_size))
     f,axes = plt.subplots(rows, columns, 
             gridspec_kw={'width_ratios':cluster_size})
     for i, cluster in enumerate(clusters_list):
-        axes[i].imshow(np.log2(np.array(cluster)).T, cmap='hot', interpolation='nearest')
+        axes[i].imshow(np.array(cluster).T, interpolation='nearest')
+        log.debug('np.array(cluster[0]).T {}'.format(np.array(cluster[0])))
         axes[i].set_yscale('log')
         axes[i].invert_yaxis()
         axes[i].get_xaxis().set_ticks([])
@@ -265,3 +336,16 @@ def main(args=None):
             axes[i].yaxis.set_visible(False)
         axes[i].set_xlabel(str(i))
     plt.savefig('density_plot.png', dpi=300)
+
+    plt.close()
+
+    colors = ['silver', 'gray', 'black', 'red', 'maroon', 'yellow', 'olive', 'lime', 'green', 'aqua', 'teal', 'blue', 'navy', 'fuchsia', 'purple']
+
+    # for i, cluster in enumerate(clusters_list):
+    #     for matrix_data in cluster:
+    #         plt.plot(list(range(len(matrix_data))), matrix_data, c=colors[i%len(colors)], alpha=0.5)
+        
+    # plt.yscale('log')
+
+    # plt.savefig('distance_coverage.png', dpi=300)
+    
