338e42ea10a376d943cbc6137484d4be0cf7c54f
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-10-10 16:57:14 +0000

    Adding spectral and kmeans, remove npz option

--- schicexplorer/scHicClusterSVL.py
+++ schicexplorer/scHicClusterSVL.py
@@ -18,7 +18,7 @@ from hicmatrix.lib import MatrixFileHandler
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
-from sklearn.cluster import SpectralClustering
+from sklearn.cluster import SpectralClustering, KMeans
 
 
 from multiprocessing import Process, Queue
@@ -37,15 +37,7 @@ def parse_arguments(args=None):
                                 help='The single cell Hi-C interaction matrices to cluster. Needs to be in mcool format',
                                 metavar='mcool scHi-C matrix',
                                 required=True)
-    parserRequired.add_argument('--matrixNpz', '-npz',
-                                help='The single cell Hi-C interaction matrices to cluster. Needs to be in mcool format',
-                                metavar='mcool scHi-C matrix',
-                                required=False)
-    parserRequired.add_argument('--createMatrix', '-cm',
-                                help='If set to, the matrix for the clustering is created out of the single cell mcool matrix. If not, the binary npz matrix of a former creation is loaded.',
-                                # metavar='Create npz matrix or load it.',
-                                # required=True,
-                                action='store_true')
+
     parserRequired.add_argument('--numberOfClusters', '-c',
                            help='Number of to be computed clusters',
                            required=False,
@@ -55,7 +47,14 @@ def parse_arguments(args=None):
                            help='Distance which should be considered as short range. Default 2MB.',
                            default=2000000,
                            type=int)
-
+    parserRequired.add_argument('--clusterMethod', '-cm',
+                           help='Algorithm to cluster the Hi-C matrices',
+                           choices=['spectral', 'kmeans'],
+                           default='spectral')
+    parserRequired.add_argument('--outFileName', '-o',
+                                help='File name to save the resulting clusters',
+                                required=True,
+                                default='clusters.txt')
     parserRequired.add_argument('--threads', '-t',
                            help='Number of threads. Using the python multiprocessing module.',
                            required=False,
@@ -69,8 +68,6 @@ def parse_arguments(args=None):
 def create_svl_data(pMatrixName, pMatricesList, pIndex, pXDimension, pDistance, pQueue):
     svl_matrix = None
     for i, matrix in enumerate(pMatricesList):
-        # matrixFileHandlerInput = MatrixFileHandler(pFileType='cool', pMatrixFile=pMatrixName+ '::' +matrix)
-        # _matrix, _, _, _, _ = matrixFileHandlerInput.load()
         chromosomes_list = cooler.Cooler(pMatrixName+ '::' +matrix).chromnames
         svl_relations = []
 
@@ -99,8 +96,6 @@ def create_svl_data(pMatrixName, pMatricesList, pIndex, pXDimension, pDistance, 
             svl_matrix = csr_matrix((pXDimension, len(chromosomes_list)), dtype=np.float)
 
         svl_matrix[pIndex+i, :] = np.array(svl_relations)
-        # if i % 20 == 0:
-        #     log.debug('pIndex + i {} {}'.format(pIndex, i))
     
     pQueue.put(svl_matrix)
 
@@ -112,75 +107,73 @@ def main(args=None):
 
     create_or_load_matrix = False
 
-    if args.createMatrix:
         
-        matrices_name = args.matrix
-        threads = args.threads
-        matrices_list = cooler.fileops.list_coolers(matrices_name)
-        svl_matrix = None
+    matrices_name = args.matrix
+    threads = args.threads
+    matrices_list = cooler.fileops.list_coolers(matrices_name)
+    svl_matrix = None
 
-        all_data_collected = False
-        thread_done = [False] * threads
-        log.debug('matrix read, starting processing')
-        length_index = [None] * threads
-        length_index[0] = 0
-        matricesPerThread = len(matrices_list) // threads
-        queue = [None] * threads
-        process = [None] * threads
-        for i in range(threads):
+    all_data_collected = False
+    thread_done = [False] * threads
+    length_index = [None] * threads
+    length_index[0] = 0
+    matricesPerThread = len(matrices_list) // threads
+    queue = [None] * threads
+    process = [None] * threads
+    for i in range(threads):
 
-            if i < threads - 1:
-                matrices_name_list = matrices_list[i * matricesPerThread:(i + 1) * matricesPerThread]
-                length_index[i + 1] = length_index[i] + len(matrices_name_list)
-            else:
-                matrices_name_list = matrices_list[i * matricesPerThread:]
+        if i < threads - 1:
+            matrices_name_list = matrices_list[i * matricesPerThread:(i + 1) * matricesPerThread]
+            length_index[i + 1] = length_index[i] + len(matrices_name_list)
+        else:
+            matrices_name_list = matrices_list[i * matricesPerThread:]
 
-            queue[i] = Queue()
-            process[i] = Process(target=create_svl_data, kwargs=dict(
-                                pMatrixName = matrices_name,
-                                pMatricesList= matrices_name_list, 
-                                pIndex = length_index[i], 
-                                pXDimension=len(matrices_list),
-                                pDistance = args.distance,
-                                pQueue=queue[i]
-                )
+        queue[i] = Queue()
+        process[i] = Process(target=create_svl_data, kwargs=dict(
+                            pMatrixName = matrices_name,
+                            pMatricesList= matrices_name_list, 
+                            pIndex = length_index[i], 
+                            pXDimension=len(matrices_list),
+                            pDistance = args.distance,
+                            pQueue=queue[i]
             )
+        )
 
-            process[i].start()
+        process[i].start()
 
-        while not all_data_collected:
-            for i in range(threads):
-                if queue[i] is not None and not queue[i].empty():
-                    csr_matrix_worker = queue[i].get()
-                    if svl_matrix is None:
-                        svl_matrix = csr_matrix_worker
-                        log.debug('returned first csr i {}'.format(i))
-                    else:
-                        svl_matrix += csr_matrix_worker
-                        log.debug('adding csr i {}'.format(i))
+    while not all_data_collected:
+        for i in range(threads):
+            if queue[i] is not None and not queue[i].empty():
+                csr_matrix_worker = queue[i].get()
+                if svl_matrix is None:
+                    svl_matrix = csr_matrix_worker
+                    log.debug('returned first csr i {}'.format(i))
+                else:
+                    svl_matrix += csr_matrix_worker
+                    log.debug('adding csr i {}'.format(i))
 
-                    queue[i] = None
-                    process[i].join()
-                    process[i].terminate()
-                    process[i] = None
-                    thread_done[i] = True
-            all_data_collected = True
-            for thread in thread_done:
-                if not thread:
-                    all_data_collected = False
-            time.sleep(1)
-        scipy.sparse.save_npz(args.matrix + '_binary.npz', svl_matrix)
-    else:
-        log.debug('read npz')
-        svl_matrix = scipy.sparse.load_npz(args.matrixNpz)
-        matrices_list = cooler.fileops.list_coolers(args.matrix)
+                queue[i] = None
+                process[i].join()
+                process[i].terminate()
+                process[i] = None
+                thread_done[i] = True
+        all_data_collected = True
+        for thread in thread_done:
+            if not thread:
+                all_data_collected = False
+        time.sleep(1)
 
-    log.debug('SVL matrix created, start clustering')
-    log.debug('len(svl_matrix) {}'.format(svl_matrix.shape))
-    # log.debug('len(svl_matrix[0]) {}'.format(len(svl_matrix[0])))
+    if args.clusterMethod == 'spectral':
+        log.debug('SVL matrix created, start clustering spectral')
+        spectral_clustering = SpectralClustering(n_clusters=args.numberOfClusters, affinity='nearest_neighbors', n_jobs=args.threads)
+        labels_clustering = spectral_clustering.fit_predict(svl_matrix)
+    elif args.clusterMethod == 'kmeans':
+        log.debug('SVL matrix created, start clustering kmeans')
 
-    spectral_clustering = SpectralClustering(n_clusters=args.numberOfClusters, affinity='nearest_neighbors', n_jobs=args.threads)
-    labels_clustering = spectral_clustering.fit_predict(svl_matrix)
+        kmeans_object = KMeans(n_clusters=args.numberOfClusters, random_state=0, n_jobs=args.threads, precompute_distances=True)
+        labels_clustering = kmeans_object.fit_predict(svl_matrix)
+    # log.debug('SVL matrix created, start clustering')
+    # log.debug('len(svl_matrix) {}'.format(svl_matrix.shape))
 
     matrices_cluster = list(zip(matrices_list, labels_clustering))
-    np.savetxt('matrices_cluster_svl.txt', matrices_cluster, fmt="%s")
+    np.savetxt(args.outFileName, matrices_cluster, fmt="%s")
