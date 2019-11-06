88d9cdfe5c51f0073b978465fd37f9ea5b477b78
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2019-11-06 17:41:51 +0000

    Test data files

---
+++ schicexplorer/test/test_scHicClusterCompartments.py
@@ -0,0 +1,9 @@
+import warnings
+warnings.simplefilter(action="ignore", category=RuntimeWarning)
+warnings.simplefilter(action="ignore", category=PendingDeprecationWarning)
+import pytest
+import os
+from tempfile import NamedTemporaryFile, mkdtemp
+
+from schicexplorer import scHicClusterCompartments
+ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "test_data/")
---
+++ schicexplorer/test/test_scHicClusterMinHash.py
@@ -0,0 +1,9 @@
+import warnings
+warnings.simplefilter(action="ignore", category=RuntimeWarning)
+warnings.simplefilter(action="ignore", category=PendingDeprecationWarning)
+import pytest
+import os
+from tempfile import NamedTemporaryFile, mkdtemp
+
+from schicexplorer import scHicClusterMinHash
+ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "test_data/")
