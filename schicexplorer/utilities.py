da1f16912e7ee3d3b9bf0f2ad8d6560db6833541
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2020-11-02 13:35:59 +0100

    Add distance option

--- schicexplorer/utilities.py
+++ schicexplorer/utilities.py
@@ -74,7 +74,7 @@ def load_matrix(pMatrix, pChromosomes, pIntraChromosomalContactsOnly, pChromosom
 
     return pixels_chromosome, shape, cooler_obj.binsize
 
-def open_and_store_matrix(pMatrixName, pMatricesList, pIndex, pXDimension, pChromosomes, pIntraChromosomalContactsOnly, pChromosomeIndices, pQueue=None):
+def open_and_store_matrix(pMatrixName, pMatricesList, pIndex, pXDimension, pChromosomes, pIntraChromosomalContactsOnly, pChromosomeIndices, pDistance=None, pQueue=None):
     neighborhood_matrix = None
     features = []
     data = []
@@ -98,9 +98,21 @@ def open_and_store_matrix(pMatrixName, pMatricesList, pIndex, pXDimension, pChro
                 if len(_matrix[2]) == 0:
                     valid_matrix_list.append(False)
                     continue
-                valid_matrix_list.append(True)
                 _matrix[0] = _matrix[0].astype(index_datatype)
                 _matrix[1] = _matrix[1].astype(index_datatype)
+
+                if pDistance is not None:
+                    distance = np.absolute(_matrix[0] - _matrix[1])
+                    mask = distance < pDistance
+                    mask_1 = pDistance > 5
+                    mask = np.logical_and(mask, mask_1)
+                    _matrix[0] = _matrix[0][mask]
+                    _matrix[1] = _matrix[1][mask]
+                    _matrix[2] = _matrix[2][mask]
+
+                valid_matrix_list.append(True)
+                # _matrix[0] = _matrix[0].astype(index_datatype)
+                # _matrix[1] = _matrix[1].astype(index_datatype)
 
                 _matrix[0] *= np.int64(shape[0])  # matrix[0] are the instance ids, matrix[3] is the shape
                 _matrix[0] += _matrix[1]  # matrix[3] is the shape, matrix[1] are the feature ids
@@ -124,7 +136,7 @@ def open_and_store_matrix(pMatrixName, pMatricesList, pIndex, pXDimension, pChro
         pQueue.put(str(exp))
 
 
-def create_csr_matrix_all_cells(pMatrixName, pThreads, pChromosomes, pOutputFolder, pRawFileName, pIntraChromosomalContactsOnly=None):
+def create_csr_matrix_all_cells(pMatrixName, pThreads, pChromosomes, pOutputFolder, pRawFileName, pIntraChromosomalContactsOnly=None, pDistance=None):
 
     matrices_name = pMatrixName
     threads = pThreads
@@ -142,13 +154,16 @@ def create_csr_matrix_all_cells(pMatrixName, pThreads, pChromosomes, pOutputFold
     process = [None] * threads
 
     chromosome_indices = None
+    cooler_obj = cooler.Cooler(pMatrixName + '::' + matrices_list[0])
     if pIntraChromosomalContactsOnly:
-        cooler_obj = cooler.Cooler(pMatrixName + '::' + matrices_list[0])
+        # cooler_obj = cooler.Cooler(pMatrixName + '::' + matrices_list[0])
         binsDataFrame = cooler_obj.bins()[:]
         chromosome_indices = {}
         for chromosome in cooler_obj.chromnames:
             chromosome_indices[chromosome] = np.array(binsDataFrame.index[binsDataFrame['chrom'] == chromosome].tolist())
 
+    if pDistance is not None:
+        pDistance = pDistance // cooler_obj.binsize
     for i in range(threads):
 
         if i < threads - 1:
@@ -166,6 +181,7 @@ def create_csr_matrix_all_cells(pMatrixName, pThreads, pChromosomes, pOutputFold
             pChromosomes=pChromosomes,
             pIntraChromosomalContactsOnly=pIntraChromosomalContactsOnly,
             pChromosomeIndices=chromosome_indices,
+            pDistance=pDistance,
             pQueue=queue[i]
         )
         )
